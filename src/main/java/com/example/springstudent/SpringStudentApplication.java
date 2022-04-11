cc7600c387ffa3e1722d82da82fb76d59cd773ec
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-04-11 10:25:26 +0000

    Jai pris attention que jai ajouté les packages dans le dossier test hhh

--- src/main/java/com/example/springstudent/SpringStudentApplication.java
+++ src/main/java/com/example/springstudent/SpringStudentApplication.java
@@ -1,13 +1,13 @@
 package com.example.springstudent;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class SpringStudentApplication {
+@SpringBootTest
+class SpringStudentApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringStudentApplication.class, args);
+    @Test
+    void contextLoads() {
     }
 
 }
