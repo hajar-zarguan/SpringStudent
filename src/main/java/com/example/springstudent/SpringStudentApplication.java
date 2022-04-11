f10a5af7cac74b168b15aa46bf97bd9e9a8e364e
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-04-11 11:25:24 +0000

    Templates - add student - updateStudent - deleteStudent

--- src/main/java/com/example/springstudent/SpringStudentApplication.java
+++ src/main/java/com/example/springstudent/SpringStudentApplication.java
@@ -1,13 +1,57 @@
 package com.example.springstudent;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringStudentApplicationTests {
+import com.example.springstudent.entities.Genre;
+import com.example.springstudent.entities.Student;
+import com.example.springstudent.repositories.StudentRepository;
+import com.example.springstudent.security.Service.SecurityService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-    @Test
-    void contextLoads() {
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudentApplication.class, args);
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    //@Bean
+    CommandLineRunner commandeLineRunner(StudentRepository studentRepository ) {
+        return args -> {
+            studentRepository.save(new Student(null,"Salwa","leila",new Date(),"@gmqil", Genre.FEMININ,true));
+            studentRepository.save(new Student(null,"anas","hilal",new Date(),"@gmqil", Genre.MASCULIN,true));
+            studentRepository.save(new Student(null,"Hanan","hilal",new Date(),"hanan@gmqil", Genre.FEMININ,true));
+            studentRepository.save(new Student(null,"anas","Omar",new Date(),"@gmqil", Genre.MASCULIN,true));
+            studentRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+        };
+    }
+    @Bean
+    CommandLineRunner saveUsers(SecurityService securityService) {
+        return args -> {
+            securityService.saveUser("fatima","1234","1234");
+            securityService.saveUser("hassan", "1234", "1234");
+
+            securityService.saveRole("USER", "role user");
+            securityService.saveRole("ADMIN", "role admin");
+
+            securityService.addRoleToUser("fatima", "USER");
+            securityService.addRoleToUser("fatima", "ADMIN");
+            securityService.addRoleToUser("hassan", "USER");
+        };
     }
 
 }
+
--- src/main/java/com/example/springstudent/Web/StudentController.java
+++ src/main/java/com/example/springstudent/Web/StudentController.java
@@ -1,4 +1,67 @@
 package com.example.springstudent.Web;
 
+import com.example.springstudent.entities.Student;
+import com.example.springstudent.repositories.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+
+@Controller
+@AllArgsConstructor
 public class StudentController {
+    private StudentRepository studentRepository ;
+    @GetMapping(path = "/user/index")
+    public String students(Model model,
+                           @RequestParam(name= "page", defaultValue = "0") int page,
+                           @RequestParam(name= "size", defaultValue = "8") int size,
+                           @RequestParam(name= "keyword", defaultValue = "") String keyword) {
+
+        Page<Student> pageStudents = studentRepository.findByNomContains(keyword, PageRequest.of(page, size));
+        model.addAttribute("ListStudents",pageStudents.getContent());
+        model.addAttribute("pages", new int[pageStudents.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
+        return "students";
+    }
+    @GetMapping("/admin/delete")
+    public String delete(Long id, String keyword, int page) {
+        studentRepository.deleteById(id);
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/admin/add")
+    public String add(Model model) {
+        model.addAttribute("student",new Student());
+        return "addStudent";
+    }
+    @PostMapping("/admin/save")
+    public String save(Model model, @Valid Student student, BindingResult bindingResult,
+                       @RequestParam(defaultValue ="0" ) int page,
+                       @RequestParam(defaultValue ="" ) String keyword) {
+        if(bindingResult.hasErrors()) return "addStudent";
+        studentRepository.save(student);
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/admin/update")
+    public String update(Model model, Long id, int page, String keyword) {
+        Student student=studentRepository.findById(id).get();
+        model.addAttribute("student",student);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "updateStudent";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    /**/
 }
