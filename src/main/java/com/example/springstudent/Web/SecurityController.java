9042c07572a746a3b4b3c10ee36d5448ae715b8e
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-04-11 10:31:20 +0000

    Security Controller ----- 404 page

--- src/main/java/com/example/springstudent/Web/SecurityController.java
+++ src/main/java/com/example/springstudent/Web/SecurityController.java
@@ -1,4 +1,12 @@
 package com.example.springstudent.Web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SecurityController {
+    @GetMapping("/404")
+    public String notAuthorized() {
+        return "403";
+    }
 }
