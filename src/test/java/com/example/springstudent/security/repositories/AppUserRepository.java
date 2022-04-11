ab86b1d82fca3e10869e78546f75143fb88d0a5b
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-04-11 09:59:23 +0000

    Security Repositories

---
+++ src/test/java/com/example/springstudent/security/repositories/AppUserRepository.java
@@ -0,0 +1,9 @@
+package com.example.springstudent.security.repositories;
+
+
+import com.example.springstudent.security.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, String> {
+    AppUser findByUsername(String username);
+}
